7786a265fee018fb236401dfd98d9f8af25c436e
xCoder <xcoder@xCoders-iMac.local>
2023-04-16 02:14:23 +0600

    Path update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,45 @@
+import { comment } from 'postcss';
 import React from 'react';
 
 const Navbar = () => {
-    return (
-        <div>
 
-        </div>
+    const routes = [
+        {
+            "id": 1,
+            "component": "Home",
+            "path": "/Home"
+        },
+        {
+            "id": 2,
+            "component": "Services",
+            "path": "/Services"
+        },
+        {
+            "id": 3,
+            "component": "price",
+            "path": "/price"
+        },
+        {
+            "id": 4,
+            "component": "contact",
+            "path": "/contact"
+        },
+    ]
+    return (
+        <nav>
+            <div>
+                <ul>
+                    {
+                        routes.map(routes => (
+                            <li
+                                key={id}
+                                component={component}></li>
+                        ))
+                    }
+                </ul>
+            </div>
+
+        </nav>
     );
 };
 
