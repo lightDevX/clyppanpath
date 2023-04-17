1fee07e29479ee8eaedfcddeed54537a4e88ad39
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2023-04-17 09:27:42 +0600

    Barchart added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Charts from './components/Charts/Charts'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Charts />
 
     </div>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '../Link/Link';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
+
 
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
 
     const routes = [
         {
@@ -27,8 +31,17 @@ const Navbar = () => {
     ]
     return (
         <nav className=' mx-auto bg-slate-400'>
+            <div onClick={() => setOpen(!open)} className=' md:hidden'>
+                <span>
+                    {
+                        open == true ?
+                            <XMarkIcon className="h-6 w-6 text-red-600 " />
+                            : <Bars3Icon className="h-6 w-6 text-red-600 " />
+                    }
+                </span>
+            </div>
             <div className=' mr-8 ml-8 p-4'>
-                <ul className='md:flex justify-between items-center'>
+                <ul className={`md:flex justify-between items-center absolute md:static bg-slate-400 ${open ? 'top-6' : '-top-36'}`}>
                     <a href="#"><img src="#" alt="" />Clyppanpath</a>
                     <div className='grid lg:flex'>
                         {
