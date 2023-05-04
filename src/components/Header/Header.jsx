7fe86f6dbaea7e59e33180ee35315cdb61adeb29
xCoder <xcoder@xCoders-iMac.local>
2023-05-04 13:45:00 +0600

    Error page creation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,9 +5,9 @@ const Header = () => {
     return (
         <nav className=' bg-slate-400 p-3 mx-auto'>
             <div className=' container '>
-                <Link to={'#'}>Home</Link>
+                <Link to={'/'}>Home</Link>
                 <Link to={'services'}>Services</Link>
-                <Link to={'#'}>Contact</Link>
+                <Link to={'contact'}>Contact</Link>
 
             </div>
         </nav>
--- src/main.jsx
+++ src/main.jsx
@@ -8,16 +8,28 @@ import {
 import './index.css'
 import Root from './components/Root/Root'
 import Services from './components/Services/Services'
+import Home from './components/Home/Home'
+import Contact from './components/Contact/Contact'
+import Error from './components/Error/Error'
 
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <Error />,
     children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
       {
         path: '/services',
         element: <Services />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
       }
     ]
 
